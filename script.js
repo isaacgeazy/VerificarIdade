a15974331e7c598077f0038e4df6d1163490c44e
Isaac Rocha <isaacgeazy@gmail.com>
2021-11-14 17:42:26 -0300

    Finalizado

--- script.js
+++ script.js
@@ -11,12 +11,43 @@ function Verificar() {
         var idade = ano - Number(fano.value)
         res.innerHTML = `Idade calculada ${idade}`
         var genero = '';
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
         if (fsex[0].checked) {
             genero = "Homem"
+            if (idade >= 0 && idade <= 12) {
+                // criança
+                 img.setAttribute('src', 'imagens/foto-bebe-m.png');
+            } else if (idade >= 12 && idade < 24) {
+                 // Jovem
+                 img.setAttribute('src', 'imagens/foto-jovem-m.png')
+            } else if (idade < 50) {
+                 // Adulto
+                 img.setAttribute('src' , 'imagens/foto-adulto-m.png')
+            } else  {
+                 // Idoso
+                 img.setAttribute('src' , 'imagens/foto-idoso-m.png')
+            }
         } else if (fsex[1].checked) {
             genero = "Mulher"
+            if (idade >= 0 && idade <= 12) {
+                // criança
+                 img.setAttribute('src', 'imagens/foto-bebe-f.png');
+            } else if (idade >= 12 && idade < 24) {
+                 // Jovem
+                 img.setAttribute('src', 'imagens/foto-jovem-f.png')
+            } else if (idade < 50) {
+                 // Adulto
+                 img.setAttribute('src' , 'imagens/foto-adulto-f.png')
+            } else  {
+                 // Idoso
+                 img.setAttribute('src' , 'imagens/foto-idoso-f.png')
+            }
         }
         res.innerHTML = `Detectamos ${genero} de ${idade} anos!`
+        res.appendChild(img)
+        res.style.textAlign = 'center'
+        
     }
 
 }
